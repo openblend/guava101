a423e2019c1c4a06e19de7c9ba83fb046262b1ca
Ales Justin <ales.justin@gmail.com>
2012-04-24 12:04:23 +0200

    Fix bi map check.

--- src/test/java/org/openblend/guava101/test/BiMapTestCase.java
+++ src/test/java/org/openblend/guava101/test/BiMapTestCase.java
@@ -18,15 +18,19 @@ public class BiMapTestCase {
     @Test
     public void testBefore() throws Exception {
         Map<String, Person> map = new HashMap<String, Person>();
-        map.put(Constants.VRHNIKA, Constants.ALES);
-        map.put(Constants.LJUBLJANA, Constants.ANDY);
+        put(map, Constants.VRHNIKA, Constants.ALES);
+        put(map, Constants.LJUBLJANA, Constants.ANDY);
         Map<Person, String> inverse = new HashMap<Person, String>();
         for (Map.Entry<String, Person> entry : map.entrySet()) {
-            if (inverse.containsKey(entry.getValue()))
-                throw new IllegalArgumentException("Dup key: " + entry.getValue());
             inverse.put(entry.getValue(), entry.getKey());
         }
         Assert.assertEquals(Constants.VRHNIKA, inverse.get(Constants.ALES));
+    }
+
+    protected static void put(Map<String, Person> map, String key, Person value) {
+        if (map.containsValue(value))
+            throw new IllegalArgumentException("Dup value: " + value);
+        map.put(key, value);
     }
 
     @Test
